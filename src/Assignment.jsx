e302e9c1bda88e9027f11ee356ea43e3ff5398a0
Joanna Gaudyn <aska.gaudyn@gmail.com>
2021-01-20 16:31:40 +0100

    Indentation fix

--- src/Assignment.jsx
+++ src/Assignment.jsx
@@ -4,18 +4,18 @@ import './style/Assignment.scss';
 
 class Assignment extends Component {
   render() {
-    const {name, items, currentUser } = this.props;
+    const { name, items, currentUser } = this.props;
 
     return (
       <div className='assignment'>
         <h1>{name}</h1>
         <div className='items'>
           {items.map(item => {
-            return(
-              <Item 
-                key={item.product.id} 
-                type={item.type} 
-                title={item.product.title} 
+            return (
+              <Item
+                key={item.product.id}
+                type={item.type}
+                title={item.product.title}
                 product={item.product}
                 image={item.product.image}
                 currentUser={currentUser}
